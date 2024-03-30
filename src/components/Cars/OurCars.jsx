1ad7e58bb4c8ff5be364865764f25fa54a3a737b
KartikLakra1 <kitrakarkal@gmail.com>
2024-03-30 12:26:07 +0530

    our cars component updated

--- src/components/Cars/OurCars.jsx
+++ src/components/Cars/OurCars.jsx
@@ -5,37 +5,37 @@ const OurCars = () => {
   const carsData = [
     {
       id: 0,
-      img: "/src/assets/img/car1.png",
+      img: "/img/car1.png",
       name: "Cadillac Escalade",
       price: "22,440",
     },
     {
       id: 1,
-      img: "/src/assets/img/car2.png",
+      img: "/img/car2.png",
       name: "BMW 3 Series",
       price: "54,890",
     },
     {
       id: 2,
-      img: "/src/assets/img/car3.png",
+      img: "/img/car3.png",
       name: "Mercedes",
       price: "75,890",
     },
     {
       id: 3,
-      img: "/src/assets/img/car4.png",
+      img: "/img/car4.png",
       name: "BMW 7 Series",
       price: "55,789",
     },
     {
       id: 4,
-      img: "/src/assets/img/car5.png",
+      img: "/img/car5.png",
       name: "Mercedes-Benz",
       price: "95,776",
     },
     {
       id: 5,
-      img: "/src/assets/img/car6.png",
+      img: "/img/car6.png",
       name: "Range Rover",
       price: "88,450",
     },
